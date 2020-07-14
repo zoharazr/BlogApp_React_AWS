39173bddb15349c98a37c6998b4d6f0ca963b7fa
zohar <zoharazr@gmail.com>
2020-07-14 13:51:40 +0300

    aws connect

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Ampplify, { Amplify } from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
+
 
 ReactDOM.render(
   <React.StrictMode>
